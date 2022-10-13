4d243f26bcef56f1fe7b67899453cc551d996405
BestmanTAQ <bestman777taq@gmail.com>
2022-10-13 16:09:36 +0300

    The first commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class NewBehaviourScript : MonoBehaviour
+    {
+
+        [SerializeField] private float speed = 5f;
+
+
+        private void Awake()
+        {
+           
+        }
+
+
+        void Start()
+        {
+            
+        }
+
+        
+        void Update()
+        {
+            
+        }
+    }
+    
