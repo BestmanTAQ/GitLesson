f6780ff81ec6a6c16d40c6c3aa25bc82f7f04ed4
BestmanTAQ <bestman777taq@gmail.com>
2022-10-24 22:23:43 +0300

    Clean Up

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -11,12 +11,12 @@ namespace Scripts.GitLesson
         [SerializeField] private Transform groundCheckerPivot;
         [SerializeField] private float checkGroundRadius = 0.4f;
         [SerializeField] private LayerMask groundMask;
-        
+
         private CharacterController _controller;
         private float _velocity;
         private Vector3 _moveDirection;
         private bool _isGrounded;
-        
+
         private void Awake()
         {
             _controller = GetComponent<CharacterController>();
@@ -25,12 +25,12 @@ namespace Scripts.GitLesson
         private void FixedUpdate()
         {
             _isGrounded = IsOnTheGround();
-            
+
             if (_isGrounded && _velocity < 0)
             {
                 _velocity = -2;
             }
-            
+
             Move(_moveDirection);
             DoGravity();
         }
@@ -58,11 +58,11 @@ namespace Scripts.GitLesson
 
         private void DoGravity()
         {
-            _velocity += gravity * Time.fixedDeltaTime; 
-            
+            _velocity += gravity * Time.fixedDeltaTime;
+
             _controller.Move(Vector3.up * _velocity * Time.fixedDeltaTime);
         }
-        
+
         private void Jump()
         {
             _velocity = Mathf.Sqrt(jumpHeight * -2 * gravity);
--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -1,25 +1,26 @@
 using UnityEngine;
 
 namespace Scripts.GitLesson
-{ 
+{
     public class Follower : MonoBehaviour
- {
-     [SerializeField] private Transform targetTransform;
-     [SerializeField] private Vector3 offset;
-     [SerializeField] private float smoothing = 1f;
- 
-  private void FixedUpdate()
-  {
-      Move();  
-  }
+    {
+        [SerializeField] private Transform targetTransform;
+        [SerializeField] private Vector3 offset;
+        [SerializeField] private float smoothing = 1f;
 
-  private void Move()
-  {
-      Vector3 nextPosition = Vector3.Lerp(transform.position, 
-                                          targetTransform.position + offset, 
-                                          Time.fixedDeltaTime * smoothing);
+        private void FixedUpdate()
+        {
+            Move();
+        }
 
-      transform.position = nextPosition;
-  }
- }
+        private void Move()
+        {
+            Vector3 nextPosition = Vector3.Lerp(
+                transform.position,
+                targetTransform.position + offset,
+                Time.fixedDeltaTime * smoothing);
+
+            transform.position = nextPosition;
+        }
+    }
 }
